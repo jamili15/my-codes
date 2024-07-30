cc07f2804fda1b59c9aeff1e7f329b415a39a819
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-07-30 16:20:28 +0800

    update

--- lib/client/service.tsx
+++ lib/client/service.tsx
@@ -1,16 +1,14 @@
 import { createFetchAsync, makeRequest } from "./fetch";
 
-const DOMAIN_PATH = process.env.NEXT_PUBLIC_MODULE_DOMAIN_PATH || "";
-
 export const getService = (data: {}) => {
-  return makeRequest(`${DOMAIN_PATH}/api/service`, {
+  return makeRequest("/api/service", {
     method: "GET",
     data,
   });
 };
 
 export const getPostService = (data: {}) => {
-  return makeRequest(`${DOMAIN_PATH}/api/service`, {
+  return makeRequest("/api/service", {
     method: "POST",
     data: data,
   });
@@ -26,6 +24,7 @@ export const lookupService = (
   connection?: string,
   option?: ServiceOptionType
 ) => {
+  if (!connection) connection = "main";
   if (!option) option = { method: "POST" };
   if (!option.method) option.method = "POST";
   option._connection = connection;
